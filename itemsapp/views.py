c7f6970fa94064afccd9cf3d1939bb8b6838d119
John Lucero <arlujohnlucero@gmail.com>
2026-09-23 04:46:33 +0800

    Adding static files

--- itemsapp/views.py
+++ itemsapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Item
 
 # Create your views here.
@@ -7,3 +8,4 @@ def index(request):
     items = Item.objects.all()
     context = {'items':items}
     return render(request,'items/index.html',context)
+
